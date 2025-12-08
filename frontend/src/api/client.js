b9816dee80295492e4cace057860cf622fe134d6
benmbarekalaa <alaeddine.benmbarek@adad.io>
2025-12-08 19:28:54 +0100

    fix backend url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = process.env.VITE_API_URL || "http://localhost:4000/api";
+const API = process.env.VITE_API_URL || "http://3.233.221.62:4000/api";
 export const list = () => axios.get(`${API}/articles`).then((r) => r.data);
 export const get = (id) =>
   axios.get(`${API}/articles/${id}`).then((r) => r.data);
